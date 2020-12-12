d3a6215d64f2217b1f9449ea32319bef29fb3713
Archangel <michaeldavidbaskoro@ymail.com>
2020-12-12 13:27:26 +0700

    use Batik_168_500by500_Single_image dataset

--- paperspace_train.sh
+++ paperspace_train.sh
@@ -1,7 +1,7 @@
-batik_dataset_name=dtd_honeycomb_dataset 
-experiment_name=single_honeycomb_cardinal_dimensionality,kernel_size=5
+batik_dataset_name=Batik_168_500by500_Single_image 
+experiment_name=single_honeycomb_cardinal_dimensionality
 
-python train.py --dataset $batik_dataset_name --save_dir ./log/$batik_dataset_name/$experiment_name --tqdm_notebook True
+python train.py --dataset batik_dataset/$batik_dataset_name --save_dir ./log/$batik_dataset_name/$experiment_name --tqdm_notebook True
 
 msg="Training ${batik_dataset_name} with ${experiment_name}"
 
