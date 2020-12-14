6ad7876e2d3afbc7b886f864e45f6e9b457a48a4
Archangel <michaeldavidbaskoro@ymail.com>
2020-12-14 10:23:06 +0700

    updated zg_dim from 20 to 60

--- paperspace_train.sh
+++ paperspace_train.sh
@@ -1,7 +1,7 @@
 batik_dataset_name=Batik_70_500by500_augmented
-experiment_name=kernel=4,generator_leakyRelu=0.2,instance_noise_mean=0_std=0.1,label_smoothing=0.0955percent,zl_dim=60,learning_rate_g=1e-4,learning_rate_d=4e-4
+experiment_name=kernel=4,generator_leakyRelu=0.2,instance_noise_mean=0_std=0.1,label_smoothing=0.0955percent,zl_dim=60,zg_dim=60,learning_rate_g=1e-4,learning_rate_d=4e-4
 
-python train.py --dataset batik_dataset/$batik_dataset_name --save_dir ./log/$batik_dataset_name/$experiment_name --tqdm_notebook True
+python train.py --dataset batik_dataset/$batik_dataset_name --save_dir ./log/$batik_dataset_name/$experiment_name --tqdm_notebook True >> ./log/$batik_dataset_name/$experiment_name.png
 
 msg="Training ${batik_dataset_name} with ${experiment_name}"
 
