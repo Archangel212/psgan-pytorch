1c4b0c664417bead3e0b6bc9307a7c92e29499c2
Archangel <michaeldavidbaskoro@ymail.com>
2020-12-16 08:53:07 +0700

    removed dropout from g architecture, changed d to use dropout at last layer with p = 0.2

--- model.py
+++ model.py
@@ -49,7 +49,6 @@ class PSGANGenerator(nn.Module):
             layers.append(nn.ConvTranspose2d(in_channels=conv_channels[ch_index-1], out_channels=conv_channels[ch_index], kernel_size=kernel_size, stride=2, padding=1))
             layers.append(nn.BatchNorm2d(conv_channels[ch_index]))
             layers.append(nn.LeakyReLU(negative_slope=0.2, inplace=self.inplace_flag))
-            layers.append(nn.Dropout2d(p=0.25))
 
         layers.append(nn.ConvTranspose2d(in_channels=conv_channels[-2], out_channels=conv_channels[-1], kernel_size=kernel_size, stride=2, padding=1))
         layers.append(nn.Tanh())
@@ -362,6 +361,7 @@ class PSGANDiscriminator(nn.Module):
             layers.append(nn.LeakyReLU(negative_slope=0.2, inplace=self.inplace_flag))
 
         layers.append(nn.Conv2d(in_channels=conv_channels[-2], out_channels=conv_channels[-1], kernel_size=kernel_size, stride=2, padding=1))
+        layers.append(nn.Dropout2d(p=0.2))
         layers.append(nn.Sigmoid())
 
         self.discriminate = nn.Sequential(*layers)
