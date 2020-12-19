c791f9cb202809bb292bb8d85cd7342f3ca60372
Archangel <michaeldavidbaskoro@ymail.com>
2020-12-19 12:09:51 +0700

    changed kernel to use 5 instead of 4, set zp back to default 3

--- model.py
+++ model.py
@@ -46,11 +46,11 @@ class PSGANGenerator(nn.Module):
         layers = []
         
         for ch_index in range(1, len(conv_channels)-1):
-            layers.append(nn.ConvTranspose2d(in_channels=conv_channels[ch_index-1], out_channels=conv_channels[ch_index], kernel_size=kernel_size, stride=2, padding=1))
+            layers.append(nn.ConvTranspose2d(in_channels=conv_channels[ch_index-1], out_channels=conv_channels[ch_index], kernel_size=kernel_size, stride=2, padding=2))
             layers.append(nn.BatchNorm2d(conv_channels[ch_index]))
             layers.append(nn.LeakyReLU(negative_slope=0.2, inplace=self.inplace_flag))
 
-        layers.append(nn.ConvTranspose2d(in_channels=conv_channels[-2], out_channels=conv_channels[-1], kernel_size=kernel_size, stride=2, padding=1))
+        layers.append(nn.ConvTranspose2d(in_channels=conv_channels[-2], out_channels=conv_channels[-1], kernel_size=kernel_size, stride=2, padding=2))
         # layers.append(nn.Tanh())
 
         self.generator = nn.Sequential(*layers)
@@ -355,12 +355,12 @@ class PSGANDiscriminator(nn.Module):
 
         layers = []
         for ch_index in range(1, len(conv_channels)-1):
-            layers.append(nn.Conv2d(in_channels=conv_channels[ch_index-1], out_channels=conv_channels[ch_index], kernel_size=kernel_size, stride=2, padding=1))
+            layers.append(nn.Conv2d(in_channels=conv_channels[ch_index-1], out_channels=conv_channels[ch_index], kernel_size=kernel_size, stride=2, padding=2))
             if ch_index != 1:
                 layers.append(nn.BatchNorm2d(conv_channels[ch_index]))
             layers.append(nn.LeakyReLU(negative_slope=0.2, inplace=self.inplace_flag))
 
-        layers.append(nn.Conv2d(in_channels=conv_channels[-2], out_channels=conv_channels[-1], kernel_size=kernel_size, stride=2, padding=1))
+        layers.append(nn.Conv2d(in_channels=conv_channels[-2], out_channels=conv_channels[-1], kernel_size=kernel_size, stride=2, padding=2))
         layers.append(nn.Dropout2d(p=0.2))
         # layers.append(nn.Sigmoid())
 
--- train.py
+++ train.py
@@ -274,11 +274,11 @@ if __name__ == '__main__':
     # detail settings
     parser.add_argument('--zl_dim', type=int, default=60, help='size of local part noise dimension')   # set default same as author's implementation
     parser.add_argument('--zg_dim', type=int, default=60, help='size of global part noise dimension')  # set default same as author's implementation
-    parser.add_argument('--zp_dim', type=int, default=6, help='size of periodic part noise dimension') # set default same as author's implementation
+    parser.add_argument('--zp_dim', type=int, default=3, help='size of periodic part noise dimension') # set default same as author's implementation
     parser.add_argument('--mlp_hidden_dim', type=int, default=60, help='size of periodic part noise dimension')
     parser.add_argument('--spatial_size', type=int, default=5, help='size of spatial dimension')
     # for pytorch there is no pad="same", if you need use 5 or other sizes, you might need add torch.nn.functional.pad in the model.
-    parser.add_argument('--kernel_size', type=int, default=4, help='size of kernels')
+    parser.add_argument('--kernel_size', type=int, default=5, help='size of kernels')
     parser.add_argument('--layer_num', type=int, default=5, help='number of layers')
     parser.add_argument('--base_conv_channel', type=int, default=64, help='base channel number of convolution layer')
     parser.add_argument('--tile', type=int, default=None, help='')
